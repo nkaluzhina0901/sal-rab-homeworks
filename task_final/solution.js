8698802800f5cc14137853bde3ea168c1ee665a1
nkaluhzina0901 <nkaluzhina@mail.ru>
2023-02-20 02:12:29 +0300

    finel_task

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,7 +38,7 @@ function sendRequest(name, phone, address, goods, sum) {
     
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
